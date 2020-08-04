3b0392acfa8c0f5ce860b8184cd88255e150fff3
2044smile <2044smile@naver.com>
2020-08-04 20:38:56 +0900

    fix: port number

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,6 @@ STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8081',
-    'http://127.0.0.1:8081'
+    'http://localhost:8080',
+    'http://127.0.0.1:8080'
 ]
