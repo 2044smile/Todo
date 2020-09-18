4396f34304b315877265b6e0d12f9a39073e1190
2044smile <2044smile@naver.com>
2020-09-18 11:36:04 +0900

    feat: permissions

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
 from user.views import AccountModelViewSet
 
@@ -9,4 +10,8 @@ router.register(r'user', AccountModelViewSet)
 urlpatterns = [
     # Endpoints
     path('', include(router.urls)),
+
+    path('auth/token/', obtain_jwt_token),
+    path('auth/token/verify_jwt_token/', verify_jwt_token),
+    path('auth/token/refresh/', refresh_jwt_token)
 ]
--- user/views/account.py
+++ user/views/account.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from user.models import Account
+from common.permissions import IsOwner
 from user.serializers import AccountSerializer
 
 
 class AccountModelViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+    permission_classes = (IsAdminUser | IsOwner,)
